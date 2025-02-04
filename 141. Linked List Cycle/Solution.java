7a7fe3c8ed45edab684700f8acfc9aea115218f3
nishoof <88177986+nishoof@users.noreply.github.com>
2025-02-03 16:08:39 -0800

    141

---
+++ 141. Linked List Cycle/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> list = new HashSet<>();
+        while (head != null) {
+            if (list.contains(head)) {
+                return true;
+            }
+            list.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
